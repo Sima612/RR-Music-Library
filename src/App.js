390b13eef03a724363e2a715f8adb9842096fff1
Sima612 <sima101531@yahoo.com>
2022-05-04 06:00:59 -0700

    added useRef Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import React, {Fragment, useEffect, useState, useRef} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
 import AlbumView from './components/AlbumView';
 import ArtistView from './components/ArtistView';
 import { DataContext } from './context/DataContext';
+import { SearchContext } from './context/SearchContext';
 
 import './App.css';
 
@@ -12,27 +13,21 @@ function App() {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
+  let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-    if(search) {
-      const fetchData = async () => {
-        document.title = `${search} Music`
-        const response = await fetch(API_URL + search)
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    const fetchData = async () => {
+        document.title = `${term} Music`
+        const response = await fetch(API_URL + term)
         const resData = await response.json()
         if(resData.results.length > 0) {
           setData(resData.results)
-        } else { setMessage('Not Found')}
-        console.log(resData)
+        } 
       }
-      fetchData()
-    }}, [search])
-  
-
-  const handleSearch = (e, term) => {
-    e.preventDefault()
-    setSearch(term)
+    fetchData()
   }
 
   return (
@@ -40,14 +35,24 @@ function App() {
       {message}
       <Router>
         <Routes>
+
           <Route path='/' element={
             <Fragment>
-              <SearchBar handleSearch={handleSearch} />
+
+              <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+              }}>
+                <SearchBar />
+              </SearchContext.Provider>
+              
               <DataContext.Provider value={data}>
                 <Gallery />
               </DataContext.Provider>
+
             </Fragment>
           } />
+          
           <Route path='/album/:id' element={<AlbumView />} />
           <Route path='/artist/:id' element={<ArtistView />} />
         </Routes>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,15 @@
 import {useState} from 'react';
+import { useContext } from 'react';
+import { SearchContext } from '../context/SearchContext';
 
-function SearchBar (props) {
-    let [searchTerm, setSearchTerm] = useState('')
+function SearchBar () {
+    const {term, handleSearch} = useContext(SearchContext)
 
     return (
         <div>
-            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-                <input type='text' placeholder='Search Here'
-                onChange={(e) => setSearchTerm(e.target.value)}/>
+            <form>
+                <input ref={term} type='text' placeholder='Search Here'
+                onChange={(e) => handleSearch(e, term.current.value)}/>
                 <input type='submit' />
             </form>
         </div>
